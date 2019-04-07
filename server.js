7b7d20f218a6b3ccce41f9e1d1ca7b343696c27c
Michal Nedzka <michael.nedzka@gmail.com>
2019-04-07 11:31:55 +0200

    Create root Router with mocked responses for a day and a month

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
 const { PORT } = require('./constants');
+const rootRouter = require('./rootRouter');
 
 const app = express();
 
 app.use(morgan('combined'));
 app.use(express.static('public'));
+app.use(rootRouter);
 
 app.set('port', PORT);
 
 app.listen(app.get('port'), () => {
-  console.info( `express app running on port: ${app.get('port')}`);
+  console.info(`express app running on port: ${app.get('port')}`);
 });
